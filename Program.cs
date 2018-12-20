839f7b9d21af7a87c53be48edd8e3041e905d580
MIGUEL ANDUJAR NAVARRO <21560273@alumnos.uem.es>
2018-12-20 16:40:02 +0100

    tu turno

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace ConsoleApp14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");            				  Console.WriteLine("Goodbye, world!");
             Console.ReadLine();
         }
     }
